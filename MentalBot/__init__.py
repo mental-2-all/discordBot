3e187b10a50b41ca37271902c2343b0932e251aa
ed.xyz <eddietang2314@gmail.com>
2023-05-13 15:59:33 -0400

    asdf

--- MentalBot/__init__.py
+++ MentalBot/__init__.py
@@ -1,12 +1,21 @@
 import disnake
 from disnake.ext import commands, tasks
 import itertools
-import aiohttp
-import textwrap
 import time
+import aiohttp
+import openai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.getenv("OPENAI")
+openai.my_api_key = api_key
+
 
 class MentalBot(commands.Bot):
     intents = disnake.Intents.all()
+
     def __init__(self):
         self.prefix = ["m.", "m ", "M.", "M "]
 
@@ -14,15 +23,29 @@ class MentalBot(commands.Bot):
 
         super().__init__(command_prefix=self.prefix, intents=MentalBot.intents)
         self.statuss = itertools.cycle(
-            [disnake.Game('MentalHealth Simulator'), disnake.Activity(type=disnake.ActivityType.watching, name="Mentally Ill People"), disnake.Game(f'{self.prefix[0]}help'), disnake.Game("MentalHealth.io")])
-        self.remove_command('help')
-    
-    
+            [
+                disnake.Game("MentalHealth Simulator"),
+                disnake.Activity(
+                    type=disnake.ActivityType.watching, name="Mentally Ill People"
+                ),
+                disnake.Game(f"{self.prefix[0]}help"),
+                disnake.Game("MentalHealth.io"),
+            ]
+        )
+        self.remove_command("help")
+        self.message_count = 0
+
     @commands.command(name="help")
     async def help(self, ctx):
-        embed = disnake.Embed(title="Help Panel", description=self.description, color=disnake.Color.blurple())
+        embed = disnake.Embed(
+            title="Help Panel",
+            description=self.description,
+            color=disnake.Color.blurple(),
+        )
         embed.set_thumbnail(url=self.bot.user.avatar.url)
-        embed.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar.url)
+        embed.set_footer(
+            text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar.url
+        )
         await ctx.send(embed=embed)
 
     @tasks.loop(seconds=60)
@@ -34,16 +57,70 @@ class MentalBot(commands.Bot):
         self.change_status.start()
         print(f"{self.user.name} is ready")
 
-
     @commands.Cog.listener()
-    async def on_message(self, message:disnake.Message):
-        if message.author.bot:
+    async def on_message(self, message: disnake.Message):
+        if message.author.bot and message.guild is None:
             return
-        
-        data = {"content": message.content,
-                "channel_name":message.channel.name,
-                "time": time.time(),
-                "nsfw": message.channel.nsfw,
-            }
-        print(message)
-        print(message.content)
+        elif not message.author.bot and message.guild is None:
+            msgs = [
+                {
+                    "role": "system",
+                    "content": "You are a intelligent mental doctor/therapist.",
+                },
+            ]
+            messages = message.channel.history(limit=200).flatten
+            for message in messages:
+                if message.author.id == 1106761415919935609:
+                    # bot
+                    msgs.append(
+                        {"role": "doctor/therapist", "content": message.content},
+                    )
+                else:
+                    msgs.append(
+                        {"role": "user", "content": message.content},
+                    )
+
+            chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=msgs)
+            reply = chat.choices[0].message.content
+            await message.channel.send(reply)
+
+            await self.process_commands(message)
+            return
+        elif message.author.bot:
+            return
+        self.message_count += 1
+        print(self.message_count)
+
+        if self.message_count % 2 == 0:
+            self.message_count = 0
+            users = message.guild.members
+
+            async with aiohttp.ClientSession() as session:
+                for user in users:
+                    async with session.post(
+                        "http://127.0.0.1:6969/pred", json={"user": user.name}
+                    ) as response:
+                        # print(await response.text())
+                        if response.json["status"] == 200:
+                            predScore = float(response.json["pred"])
+                            if predScore < -0.5:
+                                session.post(
+                                    "http://127.0.0.1:6969/flag",
+                                    json={"user": user.name, "bool": True},
+                                )
+                        pass
+
+        data = {
+            "user": message.author.name,
+            "content": message.content,
+            "time": time.time(),
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.post(
+                "http://127.0.0.1:6969/data", json=data
+            ) as response:
+                # print(await response.text())
+                pass
+        await self.process_commands(message)
+        # print(message)
+        # print(message.content)
--- main.py
+++ main.py
@@ -3,8 +3,8 @@
 import disnake
 from disnake.ext import commands
 import os
-import dotenv 
-import textwrap 
+import dotenv
+import textwrap
 
 dotenv.load_dotenv()
 
@@ -13,27 +13,40 @@ from MentalBot import MentalBot
 
 bot = MentalBot()
 
-description = textwrap.dedent((f"""
+description = textwrap.dedent(
+    (
+        f"""
         **General**
         `m.help` - help command 
         `/help` - also a help command
-        """))
+        """
+    )
+)
+
 
 @bot.command(name="help")
 async def help(ctx):
-    embed = disnake.Embed(title="Help Panel", description=description, color=disnake.Color.blurple())
+    embed = disnake.Embed(
+        title="Help Panel", description=description, color=disnake.Color.blurple()
+    )
     embed.set_thumbnail(url=bot.user.avatar.url)
-    embed.set_footer(text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar.url)
+    embed.set_footer(
+        text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar.url
+    )
     await ctx.send(embed=embed)
+
 
 @bot.slash_command(name="help", description="A help command!")
 async def slash_help(inter):
-    embed = disnake.Embed(title="Help Panel", description=description, color=disnake.Color.blurple())
+    embed = disnake.Embed(
+        title="Help Panel", description=description, color=disnake.Color.blurple()
+    )
     embed.set_thumbnail(url=bot.user.avatar.url)
-    embed.set_footer(text=f"Requested by {inter.author.name}", icon_url=inter.author.avatar.url)
+    embed.set_footer(
+        text=f"Requested by {inter.author.name}", icon_url=inter.author.avatar.url
+    )
     await inter.response.send_message(embed=embed)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     bot.run(os.getenv("TOKEN"))
