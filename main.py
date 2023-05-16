a5addea07a3c1bd9fa66c37e4457717bbeed301f
ed.xyz <eddietang2314@gmail.com>
2023-05-16 18:54:35 -0400

    asdf

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import disnake
 import os
 import dotenv
 import textwrap
+from disnake import Option
 
 dotenv.load_dotenv()
 
@@ -67,12 +68,20 @@ async def scrapeAllMessages(ctx):
                 pass
 
 
-@bot.command(name="illness")
-async def getIllness(ctx, mention: disnake.User):
+@bot.slash_command(name="illness", guild_ids=[697155840221905014, 958488149724635146])
+async def getIllness(inter: disnake.CommandInteraction, user: disnake.User):
+    """
+    Get's illness of someone
+
+    Parameters
+    ----------
+    user: the user you want to see
+    inter: discord interaction
+    """
     pred = 0
     async with aiohttp.ClientSession() as session:
         async with session.post(
-            "http://127.0.0.1:6969/pred", json={"id": str(mention.id)}
+            "http://127.0.0.1:6969/pred", json={"id": str(user.id)}
         ) as response:
             if response.status == 200:
                 # jsonify({"status": 200, "msg": "success", "pred": index / count})
@@ -86,20 +95,20 @@ async def getIllness(ctx, mention: disnake.User):
 
     desc = ""
     if pred != 69:
-        desc = f"**idk all jokes no serious** \nOur models prediction of {mention.name}'s chances of mental illness: between -1 and 1, -1 is very ill, 1 is perfectly ok\n Pred:{pred}"
+        desc = f"**idk all jokes no serious** \nOur models prediction of {user.name}'s chances of mental illness: between -1 and 1, -1 is very ill, 1 is perfectly ok\n Pred:{pred}"
     else:
-        desc = f"{mention.name} is not in our db/processed sryy!! you first have to wait for them to start talking ;)\n or ask eddie ;)"
+        desc = f"{user.name} is not in our db/processed sryy!! you first have to wait for them to start talking ;)\n or ask eddie ;)"
 
     embed = disnake.Embed(
-        title=f"Illness of {mention.name}",
+        title=f"Illness of {user.name}",
         description=desc,
         color=disnake.Color.blurple(),
     )
     embed.set_thumbnail(url=bot.user.avatar.url)
     embed.set_footer(
-        text=f"Requested by {ctx.author.name}", icon_url=ctx.author.avatar.url
+        text=f"Requested by {inter.author.name}", icon_url=inter.author.avatar.url
     )
-    await ctx.send(embed=embed)
+    await inter.response.send_message(embed=embed)
 
 
 if __name__ == "__main__":
